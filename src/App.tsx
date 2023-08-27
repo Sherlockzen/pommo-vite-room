680793404fd9a5dfe424937f508329d25ea5b426
Hewerton <hewertonlourenco@gmail.com>
2023-08-27 14:05:16 -0300

    Starting project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className=" w-full h-screen flex items-center justify-center">
+        Starting the boilerplate
+      </div>
+    </>
+  );
+}
+
+export default App;
